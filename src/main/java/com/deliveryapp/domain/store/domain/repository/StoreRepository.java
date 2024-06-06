51796d0e82d5e071b3a54b9197a10618a12a662d
LeeJaeHyeok97 <hazardous10@naver.com>
2024-06-06 17:44:58 +0900

    feat: N+1 문제 해결(카테고리별 음식점 리스트 조회)

--- src/main/java/com/deliveryapp/domain/store/domain/repository/StoreRepository.java
+++ src/main/java/com/deliveryapp/domain/store/domain/repository/StoreRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    @Query("SELECT s FROM Store s JOIN Delivery d ON s.id = d.store.id WHERE s.detailCategory.id = :categoryId")
+    @Query("SELECT s FROM Store s JOIN FETCH s.delivery d WHERE s.detailCategory.id = :categoryId")
     List<Store> findAllByCategoryId(Long categoryId);
 
     Optional<Store> findByStoreName(String storeName);
