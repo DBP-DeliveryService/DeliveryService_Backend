97e290dd6cf4b76e3e726048d560b1a2489b6e06
LeeJaeHyeok97 <hazardous10@naver.com>
2024-06-06 15:24:46 +0900

    feat: 가게 검색기능 추가, 인덱싱 추가

--- src/main/java/com/deliveryapp/domain/store/domain/repository/StoreRepository.java
+++ src/main/java/com/deliveryapp/domain/store/domain/repository/StoreRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
     @Query("SELECT s FROM Store s JOIN Delivery d ON s.id = d.store.id WHERE s.detailCategory.id = :categoryId")
     List<Store> findAllByCategoryId(Long categoryId);
+
+    Optional<Store> findByStoreName(String storeName);
 }
--- src/main/java/com/deliveryapp/domain/store/presentation/StoreController.java
+++ src/main/java/com/deliveryapp/domain/store/presentation/StoreController.java
@@ -15,10 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,18 @@ import java.util.List;
 public class StoreController {
 
     private final StoreService storeService;
+
+    @Operation(summary = "가게명 검색", description = "가게명으로 가게 정보를 검색합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "가게 정보 조회 성공", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = SearchCategoryRes.class)))}),
+            @ApiResponse(responseCode = "400", description = "가게 정보 조회 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+    })
+    @GetMapping("/search")
+    public ResponseCustom<SearchCategoryRes> searchByStoreName(
+            @Parameter(description = "가게의 storeName을 입력해주세요.") @RequestParam String storeName
+    ) {
+        return storeService.searchStoresByStoreName(storeName);
+    }
 
     @Operation(summary = "가게 상세 조회", description = "가게 정보를 상세 조회합니다.")
     @ApiResponses(value = {
